5be48e7573686c04c0c9aba2ba20f79a2d532c5b
Zheng Qin <104722415+Beizhai404@users.noreply.github.com>
2023-04-11 18:01:44 -0400

    Create Client.java

---
+++ src/main/java/server/Client.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    private final String host;
+    private final int port;
+
+    public Client(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+  
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT)) {
+            System.out.println("Connecté au serveur");
+
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+
+            while (true) {
+                System.out.print("Entrez une commande (charger ou inscription): ");
+                String command = scanner.nextLine();
+                if (command.equalsIgnoreCase("charger")) {
+                    System.out.print("Entrez la session: ");
+                    String session = scanner.nextLine();
+                    objectOutputStream.writeObject(command + " " + session);
+                    Object response = objectInputStream.readObject();
+                    if (response instanceof String) {
+                        System.out.println((String) response);
+                    } else {
+                        System.out.println("Réponse inattendue du serveur: " + response);
+                    }
+                } else if (command.equalsIgnoreCase("inscription")) {
+                    System.out.print("Entrez la session: ");
+                    String session = scanner.nextLine();
+                    System.out.print("Entrez le code du cours: ");
+                    String codeCours = scanner.nextLine();
+                    System.out.print("Entrez le matricule: ");
+                    String matricule = scanner.nextLine();
+                    System.out.print("Entrez le prénom: ");
+                    String prenom = scanner.nextLine();
+                    System.out.print("Entrez le nom: ");
+                    String nom = scanner.nextLine();
+                    System.out.print("Entrez l'email: ");
+                    String email = scanner.nextLine();
+                    String inscription = session + "\t" + codeCours + "\t" + matricule + "\t" + prenom + "\t" + nom + "\t" + email;
+                    objectOutputStream.writeObject(command + " " + inscription);
+                    Object response = objectInputStream.readObject();
+                    if (response instanceof Boolean) {
+                        if ((Boolean) response) {
+                            System.out.println("Inscription réussie!");
+                        } else {
+                            System.out.println("Échec de l'inscription.");
+                        }
+                    } else {
+                        System.out.println("Réponse inattendue du serveur: " + response);
+                    }
+                } else {
+                    System.out.println("Commande invalide.");
+                }
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
